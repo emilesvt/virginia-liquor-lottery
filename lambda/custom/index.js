bd073cdc59a9e87e33df78f754fcccdc78d648a1
Eric Miles <eric.miles@capitalone.com>
2017-12-10 11:48:03 -0500

    Fixing certification issues

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -110,23 +110,23 @@ function retrieveLotteries() {
                 distributions.push({
                     name: name,
                     productUrl: productUrl,
-                    image: new Promise((resolve, reject) => {
-                        rp({
-                            method: "GET",
-                            uri: productUrl,
-                            headers: {
-                                "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
-                            },
-                            transform: (body) => {
-                                return cheerio.load(body);
-                            }
-                        }).then($$ => {
-                            const image = `https://www.abc.virginia.gov${$$(".img-responsive").attr("src")}`;
-                            resolve(image.substring(0, image.indexOf("?")));
-                        }).catch(e => {
-                            reject(e);
-                        });
-                    }),
+                    // image: new Promise((resolve, reject) => {
+                    //     rp({
+                    //         method: "GET",
+                    //         uri: productUrl,
+                    //         headers: {
+                    //             "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
+                    //         },
+                    //         transform: (body) => {
+                    //             return cheerio.load(body);
+                    //         }
+                    //     }).then($$ => {
+                    //         const image = `https://www.abc.virginia.gov${$$(".img-responsive").attr("src")}`;
+                    //         resolve(image.substring(0, image.indexOf("?")));
+                    //     }).catch(e => {
+                    //         reject(e);
+                    //     });
+                    // }),
                     quantity: details[1].trim(),
                     price: details[2].trim(),
                     open: details[0].indexOf("ENTER LOTTERY HERE") >= 0,
